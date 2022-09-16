74a7889276b65943919f3b5289f31d3a59f7ddc8
Gagans'pc <vijay.yadav@newvisionsoftware.in>
2022-09-16 17:40:14 +0530

    updating code bash from 7.0

--- microfrontends/strapi-config-app/src/components/StrapiSettingForm.js
+++ microfrontends/strapi-config-app/src/components/StrapiSettingForm.js
@@ -21,8 +21,10 @@ export default class StrapiSettingForm extends Component {
     validate = (value) => {
         if (!value) {
             return MSG_REQ_APPLICATION_URL;
+        } else if((value.match(" "))){
+            return MSG_VALID_APPLICATION_URL
         } else if (
-            !value.match(/(((http)|(https))?:\/\/(www)\.(\w+)\.((\w{3})|(\w{2}))$)|^((https?:\/\/))(?:([a-zA-Z]+)|(\d+\.\d+\.\d+\.\d+)):\d{4}?$/)
+            !value.match(/((^((http)|(https))?:\/\/(((\w+)\.(\w+)|)\/(((entando-strapi)?$))|((www)|(\w+))\.((\w+)|((\w+)-(\w+))|(\d+\.\d+\.\d+\.\d+))\.(((\w{3})|(\w{2}))|((\w{3}).*((\w{3})|(\w{2}))))\/(entando-strapi$)))|((https?:\/\/))(?:([a-zA-Z]+)|(\d+\.\d+\.\d+\.\d+)):\d{4}?$)/)
         ) {
             return MSG_VALID_APPLICATION_URL;
         } else {
@@ -109,7 +111,7 @@ export default class StrapiSettingForm extends Component {
                                         </sup>
                                     </span>
                                 </label>
-                                <FieldLevelHelp buttonClass="" close={undefined} content={TOOLTIP_URL} inline placement="right" rootClose />
+                                <FieldLevelHelp content={TOOLTIP_URL} inline placement="right" rootClose />
                             </Col>
                             <Col lg={5}>
                                 <input
@@ -135,14 +137,6 @@ export default class StrapiSettingForm extends Component {
                                 </button>
                             </Col>
                         </Row>
-                        {/* <Row>
-                            <Col lg={8}></Col>
-                            <Col lg={4}>
-                                <button className="btn-primary btn"
-                                    disabled={!baseUrl || error}>{BUTTON_SAVE}
-                                </button>
-                            </Col>
-                        </Row> */}
                     </Grid>
                 </form>
                 <div>
--- microfrontends/strapi-config-app/src/helpers/constants.js
+++ microfrontends/strapi-config-app/src/helpers/constants.js
@@ -1,5 +1,5 @@
 
-export const TOOLTIP_URL = 'URL of the application, Example - http://20.113.131.137:1337'
+export const TOOLTIP_URL = 'URL of the application, Example - http://20.113.131.137:1337 \n http://quickstart.192.168.43.3.nip.io/entando-strapi \n http://hubdev.okd-entando.org/entando-strapi'
 export const LABEL_APPLICATION_URL = 'Application URL'
 export const MSG_REQ_APPLICATION_URL = 'Application URL is Required'
 export const MSG_VALID_APPLICATION_URL = 'Enter a valid application URL'
